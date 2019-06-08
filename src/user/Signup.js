b533483da3189113803286f3d5e9e400ece5b3b7
LeoFigueredo <leonardofprada@gmail.com>
2019-06-08 18:16:02 -0400

    Adding signup route.

---
+++ src/user/Signup.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Signup extends Component {
+    state = {  }
+    render() {
+        return (
+            <div>
+                <h2>Signup</h2>
+            </div>
+        );
+    }
+}
+
+export default Signup;
